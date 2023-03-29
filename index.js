ff2f55fe4b4b1ac0ed048b35170c7cd8e08e274c
SirreaReeves <reevessirrea@gmail.com>
2023-03-29 18:42:46 +0000

    second problem completed

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+
 function fiveToOneHundred() {
     for (let i=5; i<=100; i++)
         console.log(i)
 };
 
 fiveToOneHundred();
+
+//2.
+function multiplesOfThree() {
+    for (let i = 0; i < 100; i++){
+        if (i % 3 === 0) {
+            console.log(i)
+        }
+       }
+    }
+multiplesOfThree();
